345a590bb048dce19b5d67560d4f2e112a15e5a8
anubhav-sl <anubhav@steinnlabs.com>
2022-05-12 00:29:56 +0530

    Fix App.js lint

--- App.jsx
+++ App.jsx
@@ -1,15 +1,22 @@
+import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import { default as theme } from './theme';
+import theme from './theme';
 
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+function HomeScreen() {
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category="h1">HOME</Text>
+    </Layout>
+  );
+}
 
-export default App = () => (
-  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+function App() {
+  return (
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <HomeScreen />
+    </ApplicationProvider>
+  );
+}
+
+export default App;
