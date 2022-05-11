b4e2afc86515333216c5b21400b3f0e9c7b87066
anubhav-sl <anubhav@steinnlabs.com>
2022-05-12 00:14:04 +0530

    Add eslint, prettier husky

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { default as theme } from './theme';
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category="h1">HOME</Text>
+  </Layout>
+);
+
+export default App = () => (
+  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
