179a381923314469d4042337c15e2cce18aafdc0
anubhav-sl <anubhav@steinnlabs.com>
2022-06-04 17:21:08 +0530

    migrate ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/jsx-props-no-spreading': 'off',
-  },
+  root: true,
+  extends: '@react-native-community',
 };
